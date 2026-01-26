6ead5f3e887f43ca50b9c7ab5567c5319ef460a8
naseaoi <bf5566@qq.com>
2026-01-26 11:49:35 +0800

    修复vps部署默认使用ipv6的问题

--- server.js
+++ server.js
@@ -153,7 +153,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Mode: ${USE_WEBDAV ? 'WebDAV Proxy' : 'Local Storage'}`);
 });
